347244cb315e800e39eaf4b81ed86cccac8b917d
qxw <1290469897@qq.com>
2025-04-08 11:56:22 +0800

    接口修改

--- realtime.go
+++ realtime.go
@@ -7,30 +7,30 @@ import (
 	hhdbRpc "github.com/wckj2023/cj_hhdb_gosdk/hhdb/rpc_interface"
 )
 
-type PointStatus int32
+type PointState int32
 
 const (
-	PointStatus_kPsInit             PointStatus = 0   //初始化0-9为好的状态
-	PointStatus_kPsGood             PointStatus = 1   //正常,表示测点的值是有效的且在正常范围内。这是最常见的状态
-	PointStatus_kPsShowWarnning     PointStatus = 10  //>=10为警告状态
-	PointStatus_kPsUncertain        PointStatus = 11  //不确定,表示测点的值可能有效，但存在某种不确定性或不可靠性。这通常用于表示数据质量差、未知状态或临时问题。
-	PointStatus_kPsInavtive         PointStatus = 12  //不活动,表示测点当前处于非活动状态，可能是因为设备已停用或者该测点当前不可用。
-	PointStatus_kPsQualityIssue     PointStatus = 13  //数据质量问题,表示测点的数据存在质量问题，可能是由于噪声、干扰或其他原因导致的不准确性。
-	PointStatus_kPsDataTypeMisMatch PointStatus = 14  //数据类型不匹配
-	PointStatus_kPsOutService       PointStatus = 15  //停用,表示测点当前处于停用状态，通常是因为设备维护或其他原因。
-	PointStatus_kPsOverRange        PointStatus = 16  //超出范围,表示测点的值超出了其正常范围，通常用于指示测点的测量值超出了可接受的上限。
-	PointStatus_kPsUnderRange       PointStatus = 17  //低于范围,表示测点的值低于其正常范围，通常用于指示测点的测量值低于可接受的下限。
-	PointStatus_kPsShowError        PointStatus = 100 //>=100为错误状态
-	PointStatus_kPsNotFound         PointStatus = 101 //错误,表示测点的值无效或处于错误状态。
-	PointStatus_kPsBad              PointStatus = 102 //错误,表示测点的值无效或处于错误状态。
-	PointStatus_kPsDeviceFailure    PointStatus = 106 //设备故障,表示测点所关联的设备发生故障，无法提供有效的数据。
-	PointStatus_kPsSensorFailure    PointStatus = 107 //传感器故障,表示与测点关联的传感器发生故障，导致无法获得有效的测量数据。
-	PointStatus_kPsCommunFailure    PointStatus = 108 //通信故障,表示测点与数据源之间的通信故障，导致无法获取或更新数据。
+	PointState_kPsInit             PointState = 0   //初始化0-9为好的状态
+	PointState_kPsGood             PointState = 1   //正常,表示测点的值是有效的且在正常范围内。这是最常见的状态
+	PointState_kPsShowWarnning     PointState = 10  //>=10为警告状态
+	PointState_kPsUncertain        PointState = 11  //不确定,表示测点的值可能有效，但存在某种不确定性或不可靠性。这通常用于表示数据质量差、未知状态或临时问题。
+	PointState_kPsInavtive         PointState = 12  //不活动,表示测点当前处于非活动状态，可能是因为设备已停用或者该测点当前不可用。
+	PointState_kPsQualityIssue     PointState = 13  //数据质量问题,表示测点的数据存在质量问题，可能是由于噪声、干扰或其他原因导致的不准确性。
+	PointState_kPsDataTypeMisMatch PointState = 14  //数据类型不匹配
+	PointState_kPsOutService       PointState = 15  //停用,表示测点当前处于停用状态，通常是因为设备维护或其他原因。
+	PointState_kPsOverRange        PointState = 16  //超出范围,表示测点的值超出了其正常范围，通常用于指示测点的测量值超出了可接受的上限。
+	PointState_kPsUnderRange       PointState = 17  //低于范围,表示测点的值低于其正常范围，通常用于指示测点的测量值低于可接受的下限。
+	PointState_kPsShowError        PointState = 100 //>=100为错误状态
+	PointState_kPsNotFound         PointState = 101 //错误,表示测点的值无效或处于错误状态。
+	PointState_kPsBad              PointState = 102 //错误,表示测点的值无效或处于错误状态。
+	PointState_kPsDeviceFailure    PointState = 106 //设备故障,表示测点所关联的设备发生故障，无法提供有效的数据。
+	PointState_kPsSensorFailure    PointState = 107 //传感器故障,表示与测点关联的传感器发生故障，导致无法获得有效的测量数据。
+	PointState_kPsCommunFailure    PointState = 108 //通信故障,表示测点与数据源之间的通信故障，导致无法获取或更新数据。
 )
 
-// Enum value maps for PointStatus.
+// Enum value maps for PointState.
 var (
-	PointStatus_name = map[int32]string{
+	PointState_name = map[int32]string{
 		0:   "kPsInit",
 		1:   "kPsGood",
 		10:  "kPsShowWarnning",
@@ -48,7 +48,7 @@ var (
 		107: "kPsSensorFailure",
 		108: "kPsCommunFailure",
 	}
-	PointStatus_value = map[string]int32{
+	PointState_value = map[string]int32{
 		"kPsInit":             0,
 		"kPsGood":             1,
 		"kPsShowWarnning":     10,
@@ -68,30 +68,38 @@ var (
 	}
 )
 
-func (x PointStatus) Enum() *PointStatus {
-	p := new(PointStatus)
+func (x PointState) Enum() *PointState {
+	p := new(PointState)
 	*p = x
 	return p
 }
 
-func (x PointStatus) StrEnum(str string) PointStatus {
-	return PointStatus(PointStatus_value[str])
+func (x PointState) StrEnum(str string) PointState {
+	return PointState(PointState_value[str])
 }
 
-func (x PointStatus) String() string {
-	return PointStatus_name[int32(x)]
+func (x PointState) String() string {
+	return PointState_name[int32(x)]
 }
+
+type GeoValue struct {
+	Longitude float32 `json:"longitude" form:"longitude"` //测点值
+	Latitude  float32 `json:"latitude" form:"latitude"`   //测点值时间
+	Altitude  float32 `json:"altitude" form:"altitude"`   //测点值状态
+}
+
+type BinaryData []byte
 
 type PointValue struct {
 	Value  interface{} `json:"v" form:"v"` //测点值
 	Mstime uint64      `json:"t" form:"t"` //测点值时间
-	Status PointStatus `json:"s" form:"s"` //测点值状态
+	State  PointState  `json:"s" form:"s"` //测点值状态
 }
 
 func (pointValue *PointValue) go2grpcPointValue() (grpcValue *rpc.PointValue) {
 	var pv rpc.PointValue
 	pv.Mstime = pointValue.Mstime
-	pv.Status = int32(pointValue.Status)
+	pv.State = int32(pointValue.State)
 	switch pointValue.Value.(type) {
 	case bool:
 		pv.Value = &rpc.PointValue_BoolValue{BoolValue: pointValue.Value.(bool)}
@@ -115,8 +123,15 @@ func (pointValue *PointValue) go2grpcPointValue() (grpcValue *rpc.PointValue) {
 		pv.Value = &rpc.PointValue_DwordValue{DwordValue: pointValue.Value.(uint32)}
 	case uint64:
 		pv.Value = &rpc.PointValue_QwordValue{QwordValue: pointValue.Value.(uint64)}
+	case GeoValue:
+		pv.Value = &rpc.PointValue_GeoValue{&rpc.GeoValue{
+			Longitude: pointValue.Value.(GeoValue).Longitude,
+			Latitude:  pointValue.Value.(GeoValue).Latitude,
+			Altitude:  pointValue.Value.(GeoValue).Altitude}}
 	case string:
 		pv.Value = &rpc.PointValue_StringValue{StringValue: []byte(pointValue.Value.(string))}
+	case BinaryData:
+		pv.Value = &rpc.PointValue_BinaryValue{BinaryValue: []byte(pointValue.Value.(string))}
 	case []bool:
 		pv.Value = &rpc.PointValue_BoolArr{BoolArr: &rpc.BoolArr{ArrValue: pointValue.Value.([]bool)}}
 	case []float32:
@@ -163,21 +178,13 @@ func (pointValue *PointValue) go2grpcPointValue() (grpcValue *rpc.PointValue) {
 		pv.Value = &rpc.PointValue_DwordArr{DwordArr: &rpc.DwordArr{ArrValue: pointValue.Value.([]uint32)}}
 	case []uint64:
 		pv.Value = &rpc.PointValue_QwordArr{QwordArr: &rpc.QwordArr{ArrValue: pointValue.Value.([]uint64)}}
-	case []string:
-		{
-			tempValue := rpc.StringArr{}
-			for _, v := range pointValue.Value.([]string) {
-				tempValue.ArrValue = append(tempValue.ArrValue, []byte(v))
-			}
-			pv.Value = &rpc.PointValue_StringArr{StringArr: &tempValue}
-		}
 	}
 	return &pv
 }
 
 func (pointValue *PointValue) grpc2goPointValue(grpcValue *rpc.PointValue) {
 	pointValue.Mstime = grpcValue.Mstime
-	pointValue.Status = PointStatus(grpcValue.Status)
+	pointValue.State = PointState(grpcValue.State)
 	switch grpcValue.GetValue().(type) {
 	case *rpc.PointValue_BoolValue:
 		pointValue.Value = grpcValue.GetBoolValue()
@@ -193,8 +200,12 @@ func (pointValue *PointValue) grpc2goPointValue(grpcValue *rpc.PointValue) {
 		pointValue.Value = grpcValue.GetLongValue()
 	case *rpc.PointValue_QwordValue:
 		pointValue.Value = grpcValue.GetQwordValue()
+	case *rpc.PointValue_GeoValue:
+		pointValue.Value = GeoValue{grpcValue.GetGeoValue().GetLongitude(), grpcValue.GetGeoValue().GetLatitude(), grpcValue.GetGeoValue().GetAltitude()}
 	case *rpc.PointValue_StringValue:
 		pointValue.Value = grpcValue.GetStringValue()
+	case *rpc.PointValue_BinaryValue:
+		pointValue.Value = BinaryData(grpcValue.GetBinaryValue())
 	case *rpc.PointValue_BoolArr:
 		pointValue.Value = grpcValue.GetBoolArr().GetArrValue()
 	case *rpc.PointValue_FloatArr:
@@ -209,8 +220,6 @@ func (pointValue *PointValue) grpc2goPointValue(grpcValue *rpc.PointValue) {
 		pointValue.Value = grpcValue.GetLongArr().GetArrValue()
 	case *rpc.PointValue_QwordArr:
 		pointValue.Value = grpcValue.GetQwordArr().GetArrValue()
-	case *rpc.PointValue_StringArr:
-		pointValue.Value = grpcValue.GetStringArr().GetArrValue()
 	}
 }
 
